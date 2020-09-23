7b6d263fb64575567800d3e135bb091500f03f3d
lesor9 <lesor@protonmail.coml>
2020-09-23 17:07:31 +0300

    implemented task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-
 exports.min = function min (array) {
-  return 0;
+	if (min.arguments.length == 0) return 0;
+	if (array[0] == undefined) return 0;
+
+	let minEl = array[0];
+	for (let i = 1; i < array.length; i++) {
+		if (array[i] < minEl) {
+			minEl = array[i];
+		}
+	}
+	return minEl;
 }
 
+
 exports.max = function max (array) {
-  return 0;
+    if (max.arguments.length == 0) return 0;
+	if (array[0] == undefined) return 0;
+
+	let maxEl = array[0];
+	for (let i = 1; i < array.length; i++) {
+		if (array[i] > maxEl) {
+			maxEl = array[i];
+		}
+	}
+	return maxEl;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (avg.arguments.length == 0) return 0;
+	if (array[0] == undefined) return 0;
+
+	let avgEl = 0;
+	for (let i = 0; i < array.length; i++) {
+		avgEl += array[i];
+	}
+	avgEl = avgEl / array.length;
+	return avgEl;
 }
